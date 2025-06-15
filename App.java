d19dbeae02d0841f16130ce15ea4a2953f35c1ca
Scylla <benjugarcia01@gmail.com>
2025-06-14 18:35:15 -0700

    App.java

--- App.java
+++ App.java
@@ -1,22 +1,11 @@
-/**
- * Main application class to launch the Flappy Bird game.
- * 
- * This class creates a JFrame window with specified dimensions,
- * sets up the window properties such as title, size, and close operation,
- * and adds the FlappyBird JPanel which contains the game logic and rendering.
- * 
- * The game window is centered on the screen, not resizable,
- * and made visible to start the game.
- */
 import javax.swing.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 360;
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        // frame.setVisible(true);
+
 		frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
